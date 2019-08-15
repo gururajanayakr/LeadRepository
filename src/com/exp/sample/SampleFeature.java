d35f924d22d980480568098a85fce6a2cafdbd2d
GuruRaja <GuruRaja@192.168.40.1>
2019-08-15 14:28:54 +0530

    Changes done in Feature A

--- src/com/exp/sample/SampleFeature.java
+++ src/com/exp/sample/SampleFeature.java
@@ -3,7 +3,9 @@ package com.exp.sample;
 public class SampleFeature {
 
 	public static void main(String[] args) {
-		System.out.println("Feature added by Engineer");
+		boolean value = false;
+		if(value==true)
+		System.out.println("FeatureA added by Engineer");
 
 	}
 
